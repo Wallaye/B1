81f8ae42953e6f5ea333836ffa9dd0d01213f87c
Wallaye <pavel.vashkevich03@gmail.com>
2023-08-04 21:29:01 +0300

    Added sql procedure.

--- B1Task1/B1Task1/SQL/Procedure.sql
+++ B1Task1/B1Task1/SQL/Procedure.sql
@@ -3,7 +3,9 @@ AS
 BEGIN
 DECLARE @sumInt BIGINT;
 DECLARE @doubleMean FLOAT;
-SELECT @sumInt = SUM(IntValue) FROM dbo.Tables;
+DECLARE @count BIGINT;
+SELECT @count = COUNT_BIG(*) FROM dbo.Tables;
+SELECT @sumInt =  SUM(CAST(IntValue AS BIGINT)) FROM dbo.Tables;
 SELECT @doubleMean = AVG(CAST(doubleValue AS FLOAT)) FROM dbo.Tables;
 SELECT @sumInt AS SumInt, @doubleMean AS MeanDouble;
 END;
